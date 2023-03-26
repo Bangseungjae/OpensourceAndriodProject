626f4739497af6b86fc04190f1d3f722512cba40
Bangseungjae <tmdwo5825@naver.com>
2023-03-26 21:54:09 +0900

    3주차 미션

--- app/src/main/java/bsj/io/exam03/MainActivity.java
+++ app/src/main/java/bsj/io/exam03/MainActivity.java
@@ -1,96 +1,66 @@
 package bsj.io.exam03;
 
+import android.content.res.Resources;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.CompoundButton;
 import android.widget.ImageView;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.Switch;
-import android.widget.TextView;
+import android.widget.ScrollView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    TextView text1, text2;
-    Switch switch1;
-    RadioGroup group;
-    RadioButton radio1, radio2, radio3;
-    Button button1, button2;
-    ImageView img;
+    ScrollView scrollView, scrollView2;
+    ImageView imageView, imageView2;
+    BitmapDrawable bitmap;
+    int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("안드로이드 사진 보기");
 
-        text1 = findViewById(R.id.text1);
-        switch1 = findViewById(R.id.switch1);
+        //레이아웃에 정의된 뷰 객체 참조
+        scrollView = findViewById(R.id.scrollView);
+        scrollView.setHorizontalScrollBarEnabled(true);
 
-        text2 = findViewById(R.id.text2);
-        group = findViewById(R.id.group);
-        radio1 = findViewById(R.id.radio1);
-        radio2 = findViewById(R.id.radio2);
-        radio3 = findViewById(R.id.radio3);
+        scrollView2 = findViewById(R.id.scrollView2);
+        scrollView2.setHorizontalScrollBarEnabled(true);
 
-        button1 = findViewById(R.id.button1);
-        button2 = findViewById(R.id.button2);
+        imageView = findViewById(R.id.imageView);
+        imageView2 = findViewById(R.id.imageView2);
 
-        img = findViewById(R.id.img);
+        Resources res = getResources();
+        bitmap = (BitmapDrawable) res.getDrawable(R.drawable.newjeans);
 
-        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if (switch1.isChecked() == true) {
-                    text2.setVisibility(View.VISIBLE);
-                    group.setVisibility(View.VISIBLE);
-                    button1.setVisibility(View.VISIBLE);
-                    button2.setVisibility(View.VISIBLE);
-                    img.setVisibility(View.VISIBLE);
-                } else {
-                    text2.setVisibility(View.INVISIBLE);
-                    group.setVisibility(View.INVISIBLE);
-                    button1.setVisibility(View.INVISIBLE);
-                    button2.setVisibility(View.INVISIBLE);
-                    img.setVisibility(View.INVISIBLE);
-                }
-            }
-        });
-        group.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup radioGroup, int i) {
-                switch (group.getCheckedRadioButtonId()) {
-                    case R.id.radio1:
-                        img.setImageResource(R.drawable.pie);
-                        break;
-                    case R.id.radio2:
-                        img.setImageResource(R.drawable.q10);
-                        break;
-                    case R.id.radio3:
-                        img.setImageResource(R.drawable.r11);
-                        break;
-                }
-            }
-        });
+        int bitmapwidth = bitmap.getIntrinsicWidth();
+        int bitmapheight = bitmap.getIntrinsicHeight();
 
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
+        //리소스 이미지 크기 설정
+        imageView.setImageDrawable(bitmap);
+        imageView.getLayoutParams().width = bitmapwidth;
+        imageView.getLayoutParams().height = bitmapheight;
 
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                switch1.setChecked(false);
-                radio1.setChecked(false);
-                radio2.setChecked(false);
-                radio3.setChecked(false);
-                img.setImageResource(0);
-            }
-        });
+        imageView2.setImageDrawable(bitmap);
+        imageView2.getLayoutParams().width = bitmapwidth;
+        imageView2.getLayoutParams().height = bitmapheight;
+
+        imageView2.setVisibility(View.INVISIBLE); //처음에는 밑에 안보이게게
+    }
+    public void onClickup(View v){
+        changeImageUp();
+    }
+    public void onClickdown(View v){
+        changeImageDown();
+    }
+
+    public void changeImageUp(){
+        imageView2.setVisibility(View.INVISIBLE);
+        imageView.setVisibility(View.VISIBLE);
+    }
+
+    public void changeImageDown() {
+        imageView.setVisibility(View.INVISIBLE);
+        imageView2.setVisibility(View.VISIBLE);
     }
 }
