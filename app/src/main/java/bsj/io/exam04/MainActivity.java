14762202239c9034b430e5e22f86f5aa5ae9977a
Bangseungjae <tmdwo5825@naver.com>
2023-04-02 17:25:51 +0900

    do_it-02

--- app/src/main/java/bsj/io/exam04/MainActivity.java
+++ app/src/main/java/bsj/io/exam04/MainActivity.java
@@ -1,8 +1,10 @@
-package bsj.io.exam03;
+package bsj.io.exam04;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import bsj.io.exam03.R;
 
 public class MainActivity extends AppCompatActivity {
 
