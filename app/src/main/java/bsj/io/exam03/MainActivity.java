206589530b65b2d9e3ff78bc0328455957c59c79
Bangseungjae <tmdwo5825@naver.com>
2023-04-02 17:36:02 +0900

    do_it-04

--- app/src/main/java/bsj/io/exam03/MainActivity.java
+++ app/src/main/java/bsj/io/exam03/MainActivity.java
@@ -1,70 +1,81 @@
 package bsj.io.exam03;
 
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.res.Resources;
-import android.graphics.drawable.BitmapDrawable;
-import android.media.Image;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.ScrollView;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.UnsupportedEncodingException;
 
 public class MainActivity extends AppCompatActivity {
-    ScrollView scrollView, scrollView2;
-    ImageView imageView, imageView2;
-    BitmapDrawable bitmap;
-    int count = 0;
 
+    EditText editText;
+    TextView textView2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = (EditText) findViewById(R.id.textView);
+        textView2 = (TextView) findViewById(R.id.textView2);
+        editText.addTextChangedListener(new TextWatcher() {
+            String beforeText;
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                beforeText = s.toString();
+            }
 
-        //레이아웃에 정의된 뷰 객체 참조
-        scrollView = findViewById(R.id.scrollView);
-        scrollView.setHorizontalScrollBarEnabled(true);
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
 
-        scrollView2 = findViewById(R.id.scrollView2);
-        scrollView2.setHorizontalScrollBarEnabled(true);
-
-        imageView = findViewById(R.id.imageView);
-        imageView2 = findViewById(R.id.imageView2);
-
-        Resources res = getResources();
-        bitmap = (BitmapDrawable) res.getDrawable(R.drawable.birdmissile1);
-
-        int bitmapwidth = bitmap.getIntrinsicWidth();
-        int bitmapheight = bitmap.getIntrinsicHeight();
-
-        //리소스 이미지 크기 설정
-        imageView.setImageDrawable(bitmap);
-        imageView.getLayoutParams().width = bitmapwidth;
-        imageView.getLayoutParams().height = bitmapheight;
-
-        imageView2.setImageDrawable(bitmap);
-        imageView2.getLayoutParams().width = bitmapwidth;
-        imageView2.getLayoutParams().height = bitmapheight;
-
-
-        imageView2.setVisibility(View.INVISIBLE);
+            public void afterTextChanged(Editable s) {
+                int length = showBytes();
+                if(length > 80) editText.setText(beforeText);
+            }
+        });
     }
-
-    public void onClickup(View v){
-        changeImageUp();
+    private int showBytes(){
+        try{
+            int length = editText.getText().toString().getBytes("EUC-KR").length;
+            textView2.setText(length + " / 80 바이트");
+            return length;
+        }catch(UnsupportedEncodingException e){
+            e.printStackTrace();
+        }
+        return -1;
     }
-    public void onClickdown(View v){
-        changeImageDown();
+    public void onbuttonClose(View v){
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setMessage("정말로 종료하시겠습니까?");
+        builder.setTitle("종료 알림창")
+                .setCancelable(false)
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int i) {
+                        dialog.cancel();
+                    }
+                })
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int i) {
+                        finish();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.setTitle("종료 알림창");
+        alert.show();
     }
-
-
-    public void changeImageUp(){
-        imageView2.setVisibility(View.INVISIBLE);
-        imageView.setVisibility(View.VISIBLE);
-    }
-
-    public void changeImageDown() {
-        imageView.setVisibility(View.INVISIBLE);
-        imageView2.setVisibility(View.VISIBLE);
+    public void onbuttonSend(View v){
+        String s =  editText.getText().toString();
+        //토스트 사용
+        final Toast toast = Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG);
+        toast.show();
+        editText.setText("");
     }
 }
