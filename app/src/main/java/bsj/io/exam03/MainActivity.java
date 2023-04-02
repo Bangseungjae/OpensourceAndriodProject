e670341536d28c4dc9b8bf649580417db8afa185
Bangseungjae <tmdwo5825@naver.com>
2023-04-02 17:19:49 +0900

    do_it-05

--- app/src/main/java/bsj/io/exam03/MainActivity.java
+++ app/src/main/java/bsj/io/exam03/MainActivity.java
@@ -1,74 +1,14 @@
 package bsj.io.exam03;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.graphics.Color;
+
 import android.os.Bundle;
-import android.view.ContextMenu;
-import android.view.ContextMenu.ContextMenuInfo;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button1, button2;
-    LinearLayout baseLayout;
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("배경색 바꾸기(Java 코드)");
-        baseLayout = (LinearLayout) findViewById(R.id.baseLayout);
-        button1 = (Button) findViewById(R.id.button1);
-        registerForContextMenu(button1);
-
-        button2 = (Button) findViewById(R.id.button2);
-        registerForContextMenu(button2);
     }
-
-    @Override
-    public void onCreateContextMenu(ContextMenu menu, View v,
-                                    ContextMenuInfo menuInfo) {
-
-        super.onCreateContextMenu(menu, v, menuInfo);
-
-        if (v == button1) {
-            menu.setHeaderTitle("배경색 변경");
-
-            menu.add(0, 1, 0, "배경색 (빨강)");
-            menu.add(0, 2, 0, "배경색 (초록)");
-            menu.add(0, 3, 0, "배경색 (파랑)");
-
-        }
-        if (v == button2) {
-            menu.add(0, 4, 0, "버튼 45도 회전");
-            menu.add(0, 5, 0, "버튼 2배 확대");
-        }
-    }
-
-    @Override
-    public boolean onContextItemSelected(MenuItem item) {
-
-        switch (item.getItemId()) {
-            case 1:
-                baseLayout.setBackgroundColor(Color.RED);
-                return true;
-            case 2:
-                baseLayout.setBackgroundColor(Color.GREEN);
-                return true;
-            case 3:
-                baseLayout.setBackgroundColor(Color.BLUE);
-                return true;
-            case 4:
-                button2.setRotation(45);
-                return true;
-            case 5:
-                button2.setScaleX(2);
-                return true;
-        }
-        return false;
-    }
-
 }
