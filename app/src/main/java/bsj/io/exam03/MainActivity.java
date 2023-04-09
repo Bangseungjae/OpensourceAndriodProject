031d25c65a50629eef4393010df066b949f37e75
Bangseungjae <tmdwo5825@naver.com>
2023-04-09 18:42:12 +0900

    8-2직접 풀어보기

--- app/src/main/java/bsj/io/exam03/MainActivity.java
+++ app/src/main/java/bsj/io/exam03/MainActivity.java
@@ -7,12 +7,15 @@ import android.os.Environment;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
     Button btnPrev, btnNext;
     myPictureView myPicture;
+    TextView tvNumber;
     int curNum=0;
     File[] imageFiles = new File[0];
     String imageFname;
@@ -21,12 +24,14 @@ public class MainActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("간단 이미지 뷰어");
+        setTitle("간단 이미지 뷰어 (변경)");
         ActivityCompat.requestPermissions(this, new String[] {android.Manifest.permission.WRITE_EXTERNAL_STORAGE},MODE_PRIVATE);
 
         btnPrev = (Button) findViewById(R.id.btnPrev);
         btnNext = (Button) findViewById(R.id.btnNext);
+        tvNumber = (TextView) findViewById(R.id.tvNumber);
         myPicture = (myPictureView) findViewById(R.id.myPictureView1);
+
         File[] allFiles = new File(Environment.getExternalStorageDirectory()
                 .getAbsolutePath()+"/Pictures").listFiles();
         for (int i=0; i<allFiles.length; i++)
@@ -35,30 +40,38 @@ public class MainActivity extends AppCompatActivity {
                 imageFiles[imageFiles.length-1] = allFiles[i];
             }
         imageFname = imageFiles[curNum].toString();
-        myPicture.imagePath=imageFname;
+        myPicture.imagePath = imageFname;
+        tvNumber.setText("1" + "/" + (imageFiles.length));
 
-        btnPrev.setOnClickListener(v -> {
-            if (curNum <= 0) {
-                Toast.makeText(getApplicationContext(), "첫번째 그림입니다", Toast.LENGTH_SHORT)
-                        .show();
-            } else {
-                curNum--;
+        btnPrev.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (curNum <= 0) {
+                    curNum = imageFiles.length - 1;
+                } else {
+                    curNum--;
+                }
                 imageFname = imageFiles[curNum].toString();
                 myPicture.imagePath = imageFname;
                 myPicture.invalidate();
+                tvNumber.setText((curNum+1) + "/" + (imageFiles.length));
+
             }
         });
 
-        btnNext.setOnClickListener(v -> {
-            if (curNum >= imageFiles.length - 1) {
-                Toast.makeText(getApplicationContext(), "마지막 그림입니다", Toast.LENGTH_SHORT)
-                        .show();
-            } else {
-                curNum++;
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (curNum >= imageFiles.length - 1) {
+                    curNum = 0;
+                } else {
+                    curNum++;
+                }
                 imageFname = imageFiles[curNum].toString();
                 myPicture.imagePath = imageFname;
                 myPicture.invalidate();
+                tvNumber.setText( (curNum+1) + "/" + (imageFiles.length));
             }
         });
+
     }
+
 }
--- app/src/main/java/bsj/io/exam03/myPictureView.java
+++ app/src/main/java/bsj/io/exam03/myPictureView.java
@@ -4,18 +4,15 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import androidx.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
-import androidx.annotation.Nullable;
-
 public class myPictureView extends View {
     String imagePath = null;
-
     public myPictureView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
     }
-
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
