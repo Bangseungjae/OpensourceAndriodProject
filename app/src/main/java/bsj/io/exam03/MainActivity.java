49d24414ce71ced692a9acf3c7c73056b57b0d48
Bangseungjae <tmdwo5825@naver.com>
2023-04-09 19:11:08 +0900

    포토샵

--- app/src/main/java/bsj/io/exam03/MainActivity.java
+++ app/src/main/java/bsj/io/exam03/MainActivity.java
@@ -1,117 +1,136 @@
 package bsj.io.exam03;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.graphics.Color;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Paint;
-import android.graphics.Rect;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.MotionEvent;
-import android.view.SubMenu;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    final static int LINE = 1, CIRCLE = 2, RECTANGLE = 3;
-    static int curShape = LINE;
-    static int curColor = Color.DKGRAY;
+    ImageButton ibZoomin, ibZoomout, ibRotate, ibBright, ibDark, ibGray;
+    MyGraphicView graphicView;
+    static float scaleX = 1, scaleY = 1;
+    static float angle = 0;
+    static float color = 1;
+    static float satur = 1;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new MyGraphicView(this));
-        setTitle("간단 그림판 (개선)");
+        setContentView(R.layout.activity_main);
+        setTitle("방승재의(2020039110) 미니 포토샵");
+
+        LinearLayout pictureLayout = (LinearLayout)findViewById(R.id.pictureLayout);
+        graphicView = (MyGraphicView) new MyGraphicView(this);
+        pictureLayout.addView(graphicView);
+//        setContentView(new MyGraphicView(this));
+        clickIcons(); // 6개 아이콘을 클릭했을 때 수행할 기능을 메소드로 정의함
+    } //onCreate
+
+    private void clickIcons() {
+        ibZoomin = (ImageButton)findViewById(R.id.ibZoomin);
+        ibZoomin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scaleX += 0.2f;
+                scaleY += 0.2f;
+                graphicView.invalidate();
+            }
+        }); // 확대아이콘 클릭리스너
+
+        ibZoomout = (ImageButton)findViewById(R.id.ibZoomout);
+        ibZoomout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scaleX -= 0.2f;
+                scaleY -= 0.2f;
+                graphicView.invalidate();
+            }
+        }); // 축소아이콘 클릭리스너
+
+        ibRotate = (ImageButton)findViewById(R.id.ibRotate);
+        ibRotate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                angle += 20;
+                graphicView.invalidate();
+            }
+        }); // 회전아이콘 클릭리스너
+
+        ibBright = (ImageButton)findViewById(R.id.ibBright);
+        ibBright.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                color += 0.2f;
+                graphicView.invalidate();
+            }
+        }); // 밝기아이콘 클릭리스너
+
+        ibDark = (ImageButton)findViewById(R.id.ibDark);
+        ibDark.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                color -= 0.2f;
+                graphicView.invalidate();
+            }
+        }); // 어둡게아이콘 클릭리스너
+
+        ibGray = (ImageButton)findViewById(R.id.ibGray);
+        ibGray.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (satur == 0) {
+                    satur = 1;
+                }
+                else satur = 0;
+                graphicView.invalidate();
+            }
+        }); // 흑백아이콘 클릭리스너
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-        menu.add(0, 1, 0, "선 그리기");
-        menu.add(0, 2, 0, "원 그리기");
-        menu.add(0, 3, 0, "사각형 그리기");
-        SubMenu sMenu = menu.addSubMenu("색상 변경 >>");
-        sMenu.add(0, 4, 0, "빨강");
-        sMenu.add(0, 5, 0, "초록");
-        sMenu.add(0, 6, 0, "파랑");
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // TODO Auto-generated method stub
-        switch (item.getItemId()) {
-            case 1:
-                curShape = LINE; // 선
-                return true;
-            case 2:
-                curShape = CIRCLE; // 원
-                return true;
-            case 3:
-                curShape = RECTANGLE; // 사각형
-                return true;
-            case 4:
-                curColor = Color.RED;
-                return true;
-            case 5:
-                curColor = Color.GREEN;
-                return true;
-            case 6:
-                curColor = Color.BLUE;
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 
     private static class MyGraphicView extends View {
-        int startX = -1, startY = -1, stopX = -1, stopY = -1;
-
         public MyGraphicView(Context context) {
             super(context);
         }
 
         @Override
-        public boolean onTouchEvent(MotionEvent event) {
-            switch (event.getAction()) {
-                case MotionEvent.ACTION_DOWN:
-                    startX = (int) event.getX();
-                    startY = (int) event.getY();
-                    break;
-                case MotionEvent.ACTION_MOVE:
-                case MotionEvent.ACTION_UP:
-                    stopX = (int) event.getX();
-                    stopY = (int) event.getY();
-                    this.invalidate();
-                    break;
-            }
-            return true;
-        }
-
         protected void onDraw(Canvas canvas) {
             super.onDraw(canvas);
+
+            int cenX = this.getWidth() / 2;
+            int cenY = this.getHeight() / 2;
+            canvas.scale(scaleX, scaleY, cenX, cenY);
+            canvas.rotate(angle, cenX, cenY);
+
             Paint paint = new Paint();
-            paint.setAntiAlias(true);
-            paint.setStrokeWidth(5);
-            paint.setStyle(Paint.Style.STROKE);
-            paint.setColor(curColor);
+            float[] array = {
+                    color, 0, 0, 0, 0,
+                    0, color, 0, 0, 0,
+                    0, 0, color, 0, 0,
+                    0, 0, 0, 1, 0
+            };
+            ColorMatrix cm = new ColorMatrix(array);
 
-            switch (curShape) {
-                case LINE:
-                    canvas.drawLine(startX, startY, stopX, stopY, paint);
-                    break;
-                case CIRCLE:
-                    int radius = (int) Math.sqrt(Math.pow(stopX - startX, 2)
-                            + Math.pow(stopY - startY, 2));
-                    canvas.drawCircle(startX, startY, radius, paint);
-                    break;
-                case RECTANGLE:
-                    Rect rect = new Rect(startX, startY, stopX, stopY);
-                    canvas.drawRect(rect, paint);
-                    break;
-            }
+            if(satur == 0)
+                cm.setSaturation(satur);
+
+            paint.setColorFilter(new ColorMatrixColorFilter(cm)); // paint 이게 satur 밑에 있어야 작동함!
+
+            Bitmap picture = BitmapFactory.decodeResource(getResources(), R.drawable.newjeans);
+            int picX = (this.getWidth() - picture.getWidth()) / 2;
+            int picY = (this.getHeight() - picture.getHeight()) / 2;
+            canvas.drawBitmap(picture, picX, picY, paint);
+            picture.recycle();
         }
-
     }
 }
