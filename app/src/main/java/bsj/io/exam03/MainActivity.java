8d10ed320c3f4348d7fa58edf0e779601cd04b6d
Bangseungjae <tmdwo5825@naver.com>
2023-04-09 18:59:38 +0900

    직접 풀어보기 9-1

--- app/src/main/java/bsj/io/exam03/MainActivity.java
+++ app/src/main/java/bsj/io/exam03/MainActivity.java
@@ -5,54 +5,113 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.RectF;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.SubMenu;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    final static int LINE = 1, CIRCLE = 2, RECTANGLE = 3;
+    static int curShape = LINE;
+    static int curColor = Color.DKGRAY;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(new MyGraphicView(this));
+        setTitle("간단 그림판 (개선)");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        menu.add(0, 1, 0, "선 그리기");
+        menu.add(0, 2, 0, "원 그리기");
+        menu.add(0, 3, 0, "사각형 그리기");
+        SubMenu sMenu = menu.addSubMenu("색상 변경 >>");
+        sMenu.add(0, 4, 0, "빨강");
+        sMenu.add(0, 5, 0, "초록");
+        sMenu.add(0, 6, 0, "파랑");
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // TODO Auto-generated method stub
+        switch (item.getItemId()) {
+            case 1:
+                curShape = LINE; // 선
+                return true;
+            case 2:
+                curShape = CIRCLE; // 원
+                return true;
+            case 3:
+                curShape = RECTANGLE; // 사각형
+                return true;
+            case 4:
+                curColor = Color.RED;
+                return true;
+            case 5:
+                curColor = Color.GREEN;
+                return true;
+            case 6:
+                curColor = Color.BLUE;
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private static class MyGraphicView extends View {
+        int startX = -1, startY = -1, stopX = -1, stopY = -1;
+
         public MyGraphicView(Context context) {
             super(context);
         }
 
         @Override
+        public boolean onTouchEvent(MotionEvent event) {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    startX = (int) event.getX();
+                    startY = (int) event.getY();
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                case MotionEvent.ACTION_UP:
+                    stopX = (int) event.getX();
+                    stopY = (int) event.getY();
+                    this.invalidate();
+                    break;
+            }
+            return true;
+        }
+
         protected void onDraw(Canvas canvas) {
             super.onDraw(canvas);
             Paint paint = new Paint();
             paint.setAntiAlias(true);
+            paint.setStrokeWidth(5);
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setColor(curColor);
 
-            paint.setStrokeWidth(60);
-            canvas.drawLine(60, 60, 600, 60, paint);
-
-            paint.setStrokeCap(Paint.Cap.ROUND);
-            canvas.drawLine(60, 160, 600, 160, paint);
-
-            RectF rectF = new RectF();
-
-            rectF.set(120, 240, 120 + 400, 200 + 200);
-            canvas.drawOval(rectF, paint);
-
-            rectF.set(120, 340, 120 + 200, 340 + 200);
-            canvas.drawArc(rectF, 40, 110, true, paint);
-
-            paint.setColor(Color.BLUE);
-            rectF.set(120, 560, 120 + 200, 560 + 200);
-            canvas.drawRect(rectF, paint);
-
-            paint.setColor(Color.argb(0x88, 0xff, 0x00, 0x00));
-            rectF.set(180, 620, 180 + 200, 620 + 200);
-            canvas.drawRect(rectF, paint);
-
+            switch (curShape) {
+                case LINE:
+                    canvas.drawLine(startX, startY, stopX, stopY, paint);
+                    break;
+                case CIRCLE:
+                    int radius = (int) Math.sqrt(Math.pow(stopX - startX, 2)
+                            + Math.pow(stopY - startY, 2));
+                    canvas.drawCircle(startX, startY, radius, paint);
+                    break;
+                case RECTANGLE:
+                    Rect rect = new Rect(startX, startY, stopX, stopY);
+                    canvas.drawRect(rect, paint);
+                    break;
+            }
         }
+
     }
-
 }
-
-
