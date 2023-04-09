8d914558ababda5965072090f865958ac16f2296
Bangseungjae <tmdwo5825@naver.com>
2023-04-09 16:57:41 +0900

    8-1

--- app/src/main/java/bsj/io/exam03/MainActivity.java
+++ app/src/main/java/bsj/io/exam03/MainActivity.java
@@ -1,81 +1,27 @@
 package bsj.io.exam03;
 
-
-import androidx.appcompat.app.AlertDialog;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.DialogInterface;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import java.io.UnsupportedEncodingException;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText editText;
-    TextView textView2;
+    DatePicker dp;
+    EditText editDiary;
+    Button btnWrite;
+    String fileName;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editText = (EditText) findViewById(R.id.textView);
-        textView2 = (TextView) findViewById(R.id.textView2);
-        editText.addTextChangedListener(new TextWatcher() {
-            String beforeText;
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                beforeText = s.toString();
-            }
+        setTitle("방승재의 간단 일기장");
 
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-            }
-
-            public void afterTextChanged(Editable s) {
-                int length = showBytes();
-                if(length > 80) editText.setText(beforeText);
-            }
-        });
-    }
-    private int showBytes(){
-        try{
-            int length = editText.getText().toString().getBytes("EUC-KR").length;
-            textView2.setText(length + " / 80 바이트");
-            return length;
-        }catch(UnsupportedEncodingException e){
-            e.printStackTrace();
-        }
-        return -1;
-    }
-    public void onbuttonClose(View v){
-        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-        builder.setMessage("정말로 종료하시겠습니까?");
-        builder.setTitle("종료 알림창")
-                .setCancelable(false)
-                .setNegativeButton("No", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int i) {
-                        dialog.cancel();
-                    }
-                })
-                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int i) {
-                        finish();
-                    }
-                });
-        AlertDialog alert = builder.create();
-        alert.setTitle("종료 알림창");
-        alert.show();
-    }
-    public void onbuttonSend(View v){
-        String s =  editText.getText().toString();
-        //토스트 사용
-        final Toast toast = Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG);
-        toast.show();
-        editText.setText("");
+        dp = (DatePicker) findViewById(R.id.datePicker1);
+        editDiary = (EditText) findViewById(R.id.edtDiary);
+        btnWrite = (Button) findViewById(R.id.btnWrite);
     }
 }
