d74f008527fa357d5a6ce54d5ca7ca164bbdd02e
Bangseungjae <tmdwo5825@naver.com>
2023-04-16 17:41:38 +0900

    직접풀어보기10-3

--- app/src/main/java/bsj/io/exam03/MainActivity.java
+++ app/src/main/java/bsj/io/exam03/MainActivity.java
@@ -1,62 +1,79 @@
 package bsj.io.exam03;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity2";
+
+    private Button btnDial, btnEnd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("명화 선호도 투표 (개선)");
+        setTitle("액티비티 테스트 예제");
 
-        final int voteCount[] = new int[9];
-        for (int i = 0; i < 9; i++)
-            voteCount[i] = 0;
-        // 9개의 이미지 버튼 객체배열
-        ImageView image[] = new ImageView[9];
-        // 9개의 이미지버튼 ID 배열
-        Integer imageId[] = { R.id.iv1, R.id.iv2, R.id.iv3, R.id.iv4, R.id.iv5,
-                R.id.iv6, R.id.iv7, R.id.iv8, R.id.iv9 };
+        Log.i(TAG, "onCreate()");
 
-        final String imgName[] = { "독서하는 소녀", "꽃장식 모자 소녀", "부채를 든 소녀",
-                "이레느깡 단 베르양", "잠자는 소녀", "테라스의 두 자매", "피아노 레슨", "피아노 앞의 소녀들",
-                "해변에서" };
-
-        for (int i = 0; i < imageId.length; i++) {
-            final int index; // 주의! 꼭 필요함..
-            index = i;
-            image[index] = (ImageView) findViewById(imageId[index]);
-            image[index].setOnClickListener(new View.OnClickListener() {
-                public void onClick(View v) {
-                    // TODO Auto-generated method stub
-                    // 투표수 증가.
-                    voteCount[index]++;
-                    Toast.makeText(getApplicationContext(),
-                            imgName[index] + ": 총 " + voteCount[index] + " 표",
-                            Toast.LENGTH_SHORT).show();
-                }
-            });
-        }
-
-        Button btnFinish = (Button) findViewById(R.id.btnResult);
-        btnFinish.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),
-                        ResultActivity.class);
-                intent.putExtra("VoteCount", voteCount);
-                intent.putExtra("ImageName", imgName);
-                startActivity(intent);
-            }
-        });
-
+        init();
+        initLr();
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart()");
+    }
+
+    public void init(){
+        btnDial = findViewById(R.id.btnDial);
+        btnEnd = findViewById(R.id.btnEnd);
+    }
+
+    public void initLr(){
+        btnDial.setOnClickListener(v ->{
+            Uri uri = Uri.parse("tel:01012345678");
+            Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+            startActivity(intent);
+        });
+        btnEnd.setOnClickListener(v ->{
+            finish();
+        });
+    }
 }
