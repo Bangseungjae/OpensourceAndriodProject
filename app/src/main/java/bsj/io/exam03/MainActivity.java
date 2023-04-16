e891babff86580ed77d2c1a01eb9deb834e42a39
Bangseungjae <tmdwo5825@naver.com>
2023-04-16 17:53:24 +0900

    도전06

--- app/src/main/java/bsj/io/exam03/MainActivity.java
+++ app/src/main/java/bsj/io/exam03/MainActivity.java
@@ -2,53 +2,35 @@ package bsj.io.exam03;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RadioGroup;
-import android.widget.Toast;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    public int number;
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("메인 엑티비티");
 
-        final EditText edtNum1 = findViewById(R.id.edtNum1);
-        final EditText edtNum2 = findViewById(R.id.edtNum2);
-        final RadioGroup rdoGroup = findViewById(R.id.rdoGroup);
-        Button btnNewActivity = findViewById(R.id.btnNewActivity);
-
-        btnNewActivity.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-
-                Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
-                if (rdoGroup.getCheckedRadioButtonId()==R.id.rdoAdd) intent.putExtra("Calc","+");
-                else if (rdoGroup.getCheckedRadioButtonId()==R.id.rdoSub) intent.putExtra("Calc","-");
-                else if (rdoGroup.getCheckedRadioButtonId()==R.id.rdoMul) intent.putExtra("Calc","*");
-                else if (rdoGroup.getCheckedRadioButtonId()==R.id.rdoDiv) intent.putExtra("Calc","/");
-
-
-                intent.putExtra("Num1",
-                        Integer.parseInt(edtNum1.getText().toString()));
-                intent.putExtra("Num2",
-                        Integer.parseInt(edtNum2.getText().toString()));
-
-                startActivityForResult(intent, 0);
+        final ProgressBar progressBar = findViewById(R.id.progressBar);
+        final EditText editText = findViewById(R.id.editText);
+        //SeekBar Listener 구현
+        SeekBar seekBar = (SeekBar) findViewById(R.id.seekBar);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+            public void onProgressChanged(SeekBar seekBar, int progress,
+                                          boolean fromUser) {
+                progressBar.setProgress(progress);
+                editText.setText(new StringBuilder().append(progress));
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
             }
         });
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK) {
-            int hap = data.getIntExtra("Hap", 0);
-            Toast.makeText(getApplicationContext(), "결과 :" + hap,
-                    Toast.LENGTH_SHORT).show();
-        }
     }
 }
