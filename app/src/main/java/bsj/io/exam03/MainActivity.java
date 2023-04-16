4edbce7936981878dc8fbc4aa340e6657b1fbecc
Bangseungjae <tmdwo5825@naver.com>
2023-04-16 17:33:57 +0900

    직접풀어보기 10-2

--- app/src/main/java/bsj/io/exam03/MainActivity.java
+++ app/src/main/java/bsj/io/exam03/MainActivity.java
@@ -1,6 +1,6 @@
 package bsj.io.exam03;
 
-import android.app.Activity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -8,51 +8,55 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("명화 선호도 투표");
+        setTitle("명화 선호도 투표 (개선)");
 
         final int voteCount[] = new int[9];
-        for(int i=0; i<9; i++) {
+        for (int i = 0; i < 9; i++)
             voteCount[i] = 0;
-            //9개의 이미지 버튼 객체배열
-        }
+        // 9개의 이미지 버튼 객체배열
         ImageView image[] = new ImageView[9];
         // 9개의 이미지버튼 ID 배열
+        Integer imageId[] = { R.id.iv1, R.id.iv2, R.id.iv3, R.id.iv4, R.id.iv5,
+                R.id.iv6, R.id.iv7, R.id.iv8, R.id.iv9 };
 
-        Integer imageId[] = { R.id.iv1, R.id.iv2, R.id.iv3, R.id.iv4,
-                R.id.iv5, R.id.iv6, R.id.iv7, R.id.iv8, R.id.iv9 };
+        final String imgName[] = { "독서하는 소녀", "꽃장식 모자 소녀", "부채를 든 소녀",
+                "이레느깡 단 베르양", "잠자는 소녀", "테라스의 두 자매", "피아노 레슨", "피아노 앞의 소녀들",
+                "해변에서" };
 
-        final String imgName[] = { "pic1", "2019038036 송빈원", "pic3",
-                "pic4", "pic5", "pic6", "pic7",
-                "pic8", "pic9"};
-
-        for (int i = 0; i<imageId.length; i++){
-            final int index;
+        for (int i = 0; i < imageId.length; i++) {
+            final int index; // 주의! 꼭 필요함..
             index = i;
             image[index] = (ImageView) findViewById(imageId[index]);
-            image[index].setOnClickListener(new View.OnClickListener(){
+            image[index].setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
-                    //투표수 증가
+                    // TODO Auto-generated method stub
+                    // 투표수 증가.
                     voteCount[index]++;
                     Toast.makeText(getApplicationContext(),
-                            imgName[index]+": 총"+ voteCount[index] +" 표",
+                            imgName[index] + ": 총 " + voteCount[index] + " 표",
                             Toast.LENGTH_SHORT).show();
                 }
             });
         }
+
         Button btnFinish = (Button) findViewById(R.id.btnResult);
-        btnFinish.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
+        btnFinish.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),
+                        ResultActivity.class);
                 intent.putExtra("VoteCount", voteCount);
                 intent.putExtra("ImageName", imgName);
                 startActivity(intent);
             }
         });
+
     }
+
 }
